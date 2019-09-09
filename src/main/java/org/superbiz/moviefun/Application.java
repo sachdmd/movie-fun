6f1a6a4e8c134f27da455e4f80ae2b65e5436355
abhinav <officialronuabhinav@gmail.com>
2019-09-09 17:46:22 +0530

    database

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -3,17 +3,25 @@ package org.superbiz.moviefun;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.superbiz.moviefun.blobstore.BlobStore;
 import org.superbiz.moviefun.blobstore.S3Store;
 import org.superbiz.moviefun.blobstore.ServiceCredentials;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class Application {
+
+    DatabaseServiceCredentials serviceCredentials;
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -46,5 +54,19 @@ public class Application {
         }
 
         return new S3Store(s3Client, photoStorageBucket);
+    }
+
+    @Bean
+    public JdbcTemplate movieJDCTemplate(DatabaseServiceCredentials serviceCredentials) {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(serviceCredentials.jdbcUrl("movies-mysql"));
+        JdbcTemplate template = new JdbcTemplate();
+        template.setDataSource(dataSource);
+        return template;
+    }
+
+    @Bean
+    public DatabaseServiceCredentials getDatabaseServiceCredentials(@Value("${VCAP_SERVICES}")String vCapService ){
+        return new DatabaseServiceCredentials(vCapService);
     }
 }
